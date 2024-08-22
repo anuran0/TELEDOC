7e57eac4b020b09fd9e17059a9e0eb9e7bd3ef90
Anuran De <anurande514@gmail.com>
2024-08-22 21:32:05 +0530

    kichu toh holo

--- client/components/Symptoms/index.jsx
+++ client/components/Symptoms/index.jsx
@@ -1,430 +1,71 @@
 import React from 'react';
+import Checkbox from './Checkbox'; 
 
 const Symptoms = () => {
+  const symptoms = [
+    'Fever',
+    'Cough',
+    'Headache',
+    'Fatigue',
+    'Shortness of Breath',
+    'Chest Pain',
+    'Dizziness',
+    'Nausea',
+    'Abdominal Pain',
+    'Joint Pain',
+    'Rash',
+    'Swollen Lymph Nodes',
+    'Sore Throat',
+    'Vomiting',
+    'Diarrhea',
+    'Constipation',
+    'Back Pain',
+    'Frequent Urination',
+    'Night Sweats',
+    'Blurred Vision',
+    'Confusion',
+    'Muscle Weakness',
+    'Paleness',
+    'Tingling or Numbness',
+    'Excessive Thirst',
+    'Hair Loss',
+    'Heart Palpitations',
+    'Loss of Appetite',
+    'Difficulty Swallowing',
+    'Dry Skin',
+    'Hearing Loss',
+    'Swollen Feet',
+    'Yellowing of Skin',
+    'Red Eyes',
+    'Blood in Stool',
+    'Blood in Urine',
+    'Dry Mouth',
+    'Chronic Cough',
+    'Sudden Weight Gain',
+    'Loss of Balance',
+    'Cold Hands/Feet',
+    'Nightmares',
+    'Sleepiness During Daytime',
+    'Leg Cramps',
+    'Bloating',
+    'Hot Flashes',
+    'Persistent Hiccups',
+    'Unexplained Bruising',
+    'Itchy Skin'
+];
+
   return (
     <>
       <div className="d-flex justify-content-center mt-3">
         Enter Your Symptoms
       </div>
       <div className="mt-2 ms-5">
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-          />
-          <label className="form-check-label" for="flexCheckDefault">
-            Fever
-          </label>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
+        {symptoms.map((symptom, index) => (
+          <div key={index} className="form-check d-flex align-items-center mb-2">
+            <Checkbox />
+            <label className="form-check-label ms-2">{symptom}</label>
           </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Headache
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Fatigue
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Shortness 
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-          <div>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" for="flexCheckDefault">
-              Cough
-            </label>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
